8c2bdbafa45483e0a6082adc2ab1d4e0a3aa0a0e
StoryTeller <db@dbonesso.dev>
2023-01-09 10:45:03 -0300

    change dirctory structure

--- tests/spark_test.py
+++ tests/spark_test.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 
 
 spark = SparkSession.builder \
-        .config("spark.hadoop.fs.s3a.endpoint", "http://10.1.112.80:9000") \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://10.1.112.89:9000") \
         .config("spark.hadoop.fs.s3a.access.key", "COQX70GCQXBBWGCSISEO") \
         .config("spark.hadoop.fs.s3a.secret.key", "Y01yFxxj9RYX4nBCGfk3xSr0RsL3T5lanjpVTz1F") \
         .config("spark.hadoop.fs.s3a.path.style.access", True) \
@@ -17,6 +17,3 @@ customer = spark.read.csv("s3a://datalake/olist_customers_dataset.csv")
 customer.show()
 print(customer.columns)
 
-male_users = userdata.select("customer_id", "customer_unique_id")
-
-#male_users.write.mode("overwrite").parquet("s3a://datalake/maleuserdata/")
--- tests/test_spark_on_k8s.py
+++
@@ -1,5 +0,0 @@
-from spark_on_k8s import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
